90d585f4310c2570a6d1164ce376a808cb67c0b7
umAbumblebee <jonahgebeyehu@gmail.com>
2026-07-26 09:56:37 +0300

    Finished debugging

--- src/features/files/pages/FileManager.jsx
+++ src/features/files/pages/FileManager.jsx
@@ -1,16 +1,12 @@
 import FileManagerProvider from "../context/FileManagerProvider";
 import FileManager from "../components/FileManager";
 
-
 function FileManagerPage() {
-
   return (
     <FileManagerProvider>
       <FileManager />
     </FileManagerProvider>
   );
-
 }
-
 
 export default FileManagerPage;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,22 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import FileManagerPage from "../features/files/pages/FileManager";
+import FileManagerPage from "../features/files/pages/FileManagerPage";
 
-
-function AppRoutes(){
-
+function AppRoutes() {
   return (
     <Routes>
+      <Route
+        path="/"
+        element={<Navigate to="/files" replace />}
+      />
 
       <Route
         path="/files"
         element={<FileManagerPage />}
       />
-
     </Routes>
   );
-
 }
-
 
 export default AppRoutes;
